4f442cee2c8155ac70bd091615578e52deb6fa6f
Dr Mohd Naved <mohdnavedresearch@gmail.com>
2025-09-04 10:15:44 +0530

    Add files via upload

---
+++ src/app/interview/page.tsx
@@ -0,0 +1,9 @@
+import InterviewClient from '@/components/interview-client';
+
+export default function InterviewPage() {
+  return (
+    <main className="min-h-screen bg-background text-foreground overflow-hidden">
+      <InterviewClient />
+    </main>
+  );
+}
